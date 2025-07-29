b77c438a0bb3386dffb637d7399faa295402dcbf
Charlie <meixiguozao@outlook.com>
2025-07-29 22:50:59 +0800

    feat(promise): 错误捕获，循环调用

--- JavaScript/手写代码/promise/myPromise.js
+++ JavaScript/手写代码/promise/myPromise.js
@@ -3,7 +3,12 @@ const FULFILLED = "fulfilled";
 const REJECTED = "rejected";
 export class MyPromise {
   constructor(executor) {
-    executor(this.resolve, this.reject);
+    // promise本身错误使用reject
+    try {
+      executor(this.resolve, this.reject);
+    } catch (error) {
+      this.reject(error);
+    }
   }
   // promise 状态
   status = PEDDING;
@@ -39,13 +44,29 @@ export class MyPromise {
     }
   };
   then(onFulfilled, onRejected) {
+    // promise2 目的是为了.then链式调用
     const promise2 = new MyPromise((resolve, reject) => {
       if (this.status === FULFILLED) {
-        const x = onFulfilled(this.value);
-        // 完成链式调用中下一个.then以来上一个.then的返回值
-        resolvePromise(x, resolve, reject);
+        // setTimeout 为了解决获取不到promise2
+        setTimeout(() => {
+          try {
+            const x = onFulfilled(this.value);
+            // 完成链式调用中下一个.then以来上一个.then的返回值
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        }, 0);
       } else if (this.status === REJECTED) {
-        onRejected(this.reason);
+        setTimeout(() => {
+          // .catch 同样也可以返回
+          try {
+            const x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error)
+          }
+        }, 0);
       } else {
         // 如果异步任务，会进入到这里，状态还是pedding
         // 把成功、失败的回调存储起来
@@ -57,12 +78,15 @@ export class MyPromise {
     return promise2;
   }
 }
-function resolvePromise(value, resolve, reject) {
+function resolvePromise(promise2, value, resolve, reject) {
+  if (promise2 === value) {
+    reject(new TypeError("不能循环调用Promise"));
+  }
   if (value instanceof MyPromise) {
     // 返回值是promise
     value.then(resolve, reject);
   } else {
-    // 返回的值是普通纸
+    // 返回的值是普通值
     resolve(value);
   }
 }
